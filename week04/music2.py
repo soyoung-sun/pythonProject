a376d8731998db034b01f86b0a6bcefef0f4588e
Soyoung Sun <dkssud02@gmail.com>
2021-12-04 10:30:41 +0900

    commit test

--- week04/music2.py
+++ week04/music2.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+# test
 def print_1st_song(date):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
